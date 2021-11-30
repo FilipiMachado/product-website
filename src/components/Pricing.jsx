cc4048b3c6929d869f04897a9879658d8ef0a8ce
Filipi <filipisee@hotmail.com>
2021-11-30 19:57:59 -0300

    added navbar

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
 // Styles
-import "./PricingStyles.css"
+import "./PricingStyles.css";
 
 const Pricing = () => {
-    return (
-        <>
-            <h1>Pricing</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="pricing">
+        <div className="container">
+          <div className="top-content">
+            <div>
+              <h2>
+                Everything you need for <span>$99 a month</span>
+              </h2>
+              <p>
+                Includes every feature we offer plus unlimited projects and
+                unlimited users.
+              </p>
+            </div>
+            <div className="btn-div">
+              <button>Get started today</button>
+            </div>
+          </div>
+          <div className="bottom-content">
+            <div className="card">
+              <p>
+                <span>Everything you need</span>
+              </p>
+              <h3>All-in-one platform</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Pricing
+export default Pricing;
